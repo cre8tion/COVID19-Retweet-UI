1e12de322aa90028f7e21142c817730c071832e9
Cre8tion <ljl1997@gmail.com>
2021-08-10 23:00:12 +0800

    Added more graphs to Overview page

--- services/load.py
+++ services/load.py
@@ -60,37 +60,35 @@ def load_data(model_number):
 
 
 def load_xgboost_feature_engineering_chart():
-    feature_scores = [
-        ("#Favourites", 41),
-        ("#Followers", 12),
-        ("confidence_mean", 12),
-        ("usernamehash_col163", 10),
-        ("usernamehash_col139", 8),
-        ("sin_second", 8),
-        ("#Friends", 7),
-        ("usernamehash_col35", 6),
-        ("usernamehash_col185", 5),
-        ("usernamehash_col132", 4),
-        ("confidence_max", 4),
-        ("usernamehash_col182", 3),
-        ("usernamehash_col54", 3),
-        ("usernamehash_col174", 3),
-        ("usernamehash_col97", 3),
-        ("Sentiments1", 3),
-        ("usernamehash_col102", 2),
-        ("usernamehash_col50", 2),
-        ("usernamehash_col73", 2),
-        ("usernamehash_col56", 2),
-        ("Sentiments0", 1),
-        ("usernamehash_col105", 1),
-        ("usernamehash_col196", 1),
-        ("Hashtag_counts", 1),
-        ("usernamehash_col201", 1),
-        ("usernamehash_col215", 1),
-    ]
+    feature_scores = [('#Favourites', 41),
+                  ('#Followers', 12),
+                  ('confidence_mean', 12),
+                  ('usernamehash_col163', 10),
+                  ('usernamehash_col139', 8),
+                  ('sin_second', 8),
+                  ('#Friends', 7),
+                  ('usernamehash_col35', 6),
+                  ('usernamehash_col185', 5),
+                  ('usernamehash_col132', 4),
+                  ('confidence_max', 4),
+                  ('usernamehash_col182', 3),
+                  ('usernamehash_col54', 3),
+                  ('usernamehash_col174', 3),
+                  ('usernamehash_col97', 3),
+                  ('Sentiments1', 3),
+                  ('usernamehash_col102', 2),
+                  ('usernamehash_col50', 2),
+                  ('usernamehash_col73', 2),
+                  ('usernamehash_col56', 2),
+                  ('Sentiments0', 1),
+                  ('usernamehash_col105', 1),
+                  ('usernamehash_col196', 1),
+                  ('Hashtag_counts', 1),
+                  ('usernamehash_col201', 1),
+                  ('usernamehash_col215', 1)]
 
     labels = [feat_score[0] for feat_score in feature_scores]
-    values = [feat_score[1] for feat_score in feature_scores]
+    values = [feat_score[1] for feat_score in feature_scores]   
 
     font = {"size": 15}
     matplotlib.rc("font", **font)
